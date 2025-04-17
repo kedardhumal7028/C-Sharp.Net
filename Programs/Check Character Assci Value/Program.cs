0483f162ab832794734426d46c980735c02f4d18
kedardhumalcts <kedardhumal31@gmail.com>
2025-04-17 19:24:59 +0530

    Programing Day 6

---
+++ Programs/Check Character Assci Value/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Check_Character_Assci_Value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            char ch1 = 'A';
+            char ch2 = 'a';
+            char ch3 = '0';
+
+            char ch4 = 'Z';
+            char ch5 = 'z';
+            char ch6 = '9';
+
+
+            Console.WriteLine("Character representation is {0} and its ASCII value is : {1}", ch1, (int)ch1);
+            Console.WriteLine("Character representation is {0} and its ASCII value is : {1}", ch2, (int)ch2);
+            Console.WriteLine("Character representation is {0} and its ASCII value is : {1}", ch3, (int)ch3);
+            Console.WriteLine();
+            Console.WriteLine("Character representation is {0} and its ASCII value is : {1}", ch4, (int)ch4);
+            Console.WriteLine("Character representation is {0} and its ASCII value is : {1}", ch5, (int)ch5);
+            Console.WriteLine("Character representation is {0} and its ASCII value is : {1}", ch6, (int)ch6);
+
+        }
+    }
+}
---
+++ Programs/Print Ascii Table/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Print_Ascii_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("ASCII TABLE");
+            Console.WriteLine();
+
+            Console.WriteLine("Character\t Decimal\t Hex\t Octal");
+            for(int i = 0; i<= 127; i++)
+            {
+                char ch = (char)i;
+                Console.WriteLine("{0}\t\t{1}\t\t{2:X}\t{3}",char.IsControl(ch) ? ' ' : ch, i, i, Convert.ToString(i, 8));
+            }
+        }
+    }
+}
